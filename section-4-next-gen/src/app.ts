3b9f93065bc13877938bcd45bb6224ec252a8bac
Yong <fkszm3@gmail.com>
2022-06-07 23:52:33 +0900

    section 04-53

--- section-4-next-gen/src/app.ts
+++ section-4-next-gen/src/app.ts
@@ -1,1 +1,44 @@
-// Code goes here!
+const userName = "Yong";
+
+// userName = "Maximilian";
+
+let age = 30;
+
+age = 29;
+
+// let result;
+// function add(a: number, b: number) {
+//   result = a + b;
+//   return result;
+// }
+
+// if (age > 20) {
+//   let isOld = true;
+// }
+
+// console.log(isOld);
+
+// const add = (a: number, b: number = 1) => a + b;
+
+// add(5);
+
+const hobbies = ["독서", "요리"];
+const activities = ["등산"];
+
+activities.push(...hobbies);
+
+const person = {
+  name: "철수",
+  age: 30,
+};
+
+const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumber = add(10, 9, 8, 7, 5, 1.3);
+console.log(addedNumber);
