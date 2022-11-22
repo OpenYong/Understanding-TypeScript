92a8f718d0ab95e44830af854392cd60e1e7e5c9
OpenYong <fkszm3@gmail.com>
2022-11-22 18:30:10 +0900

    Section 8-109 Diving into Property Decorators

--- section-8-decorators/src/app.ts
+++ section-8-decorators/src/app.ts
@@ -33,3 +33,33 @@ class Person {
 const firstPerson = new Person();
 
 console.log(firstPerson);
+
+// ------------------------------------------------------
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("프로퍼티 데코레이터");
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("유효하지 않은 가격 - 음수가 될 수 없음!");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
