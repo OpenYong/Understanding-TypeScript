6b4b84cf5bf7e920c47c9e09df2ec89764542b92
OpenYong <fkszm3@gmail.com>
2022-11-22 17:26:14 +0900

    Section 8 - 107 Building More Useful Decorators

--- section-8-decorators/src/app.ts
+++ section-8-decorators/src/app.ts
@@ -1,13 +1,27 @@
-function Logger(logString : string) {
+function Logger(logString: string) {
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
   };
 }
 
-@Logger('PERSON 로그')
+function WithTemplate(template: string, hookId: string) {
+  // constructor를 사용하지 않을때 언더스코어('_')로 이름을 설정해줄 수 있다.
+  //   return function (_: Function) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger('PERSON 로그')
+@WithTemplate("<h1>나의 Person 객체</h1>", "app")
 class Person {
-  name = "Max";
+  name = "Yong";
 
   constructor() {
     console.log("Person 객체 생성");
@@ -17,4 +31,3 @@ class Person {
 const firstPerson = new Person();
 
 console.log(firstPerson);
- 
